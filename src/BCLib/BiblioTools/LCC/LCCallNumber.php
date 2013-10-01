7e41d113b708c8ceae16e7cb635381e3c2aa0476
Ben Florin <benjamin.florin@bc.edu>
2013-10-01 17:23:48 -0400

    Move parser into LCCallNumber

--- src/BCLib/BiblioTools/LCC/LCCallNumber.php
+++ src/BCLib/BiblioTools/LCC/LCCallNumber.php
@@ -6,13 +6,15 @@ namespace BCLib\BiblioTools\LCC;
  * Class LCCN
  * @package BCLib\BiblioTools\LCC
  *
+ * Parses and normalizes Library of Congress Call Numbers, using an
+ * implementaiton shamelessly stolen from Bill Deuber.
+ *
  * @property string      letters
  * @property string      number
  * @property string      cutter_1
  * @property string      cutter_2
  * @property string      cutter_3
  * @property string      remainder
- * @property-read string input_string
  */
 class LCCallNumber
 {
@@ -28,47 +30,48 @@ class LCCallNumber
 
     protected $_is_valid = false;
 
+    protected $_matches = array();
+
     const HI_SORT_CHAR = '~';
     const LOW_SORT_CHAR = ' ';
 
-    /** @var  LCCParser */
-    protected $_parser;
-
-    public function __construct(LCCParser $parser)
-    {
-        $this->_parser = $parser;
-    }
-
     public function normalize()
     {
-        $normalized = str_pad($this->_letters, 3, LCCallNumber::LOW_SORT_CHAR, STR_PAD_RIGHT);
+        return str_pad($this->_letters, 3, LCCallNumber::LOW_SORT_CHAR, STR_PAD_RIGHT) .
+        $this->_normalizeNumber() .
+        $this->_normalizeCutters() .
+        $this->_normalizeRemainder();
+    }
 
+    protected function _normalizeNumber()
+    {
         // Only normalize numbers to \d\d\d\d.\d\d
         list($pre_dec, $post_dec) = explode('.', $this->_number);
         $pre_dec = substr($pre_dec, 0, 5);
         $post_dec = substr($post_dec, 0, 3);
-        $normalized .= str_pad($pre_dec, 5, LCCallNumber::LOW_SORT_CHAR, STR_PAD_LEFT);
-        $normalized .= str_pad($post_dec, 3, LCCallNumber::LOW_SORT_CHAR, STR_PAD_RIGHT);
+        $normalized = str_pad($pre_dec, 5, LCCallNumber::LOW_SORT_CHAR, STR_PAD_LEFT);
+        return $normalized . str_pad($post_dec, 3, LCCallNumber::LOW_SORT_CHAR, STR_PAD_RIGHT);
+    }
+
+    protected function _normalizeCutters()
+    {
+        $normalized = '';
 
         for ($i = 1; $i <= 3; $i++) {
             if (isset($this->_cutters[$i])) {
-                $normalized .= $this->_normalizeCutter($i);
-            }
-            else {
+                $cutter = substr($this->_cutters[$i], 0, 4);
+                $normalized .= str_pad($cutter, 4, LCCallNumber::LOW_SORT_CHAR, STR_PAD_RIGHT);
+            } else {
                 $normalized .= str_repeat(LCCallNumber::LOW_SORT_CHAR, 4);
             }
         }
-
-        $remainder = substr($this->_remainder, 0, 5);
-        $normalized .= str_pad($remainder, 5, LCCallNumber::LOW_SORT_CHAR, STR_PAD_RIGHT);
-
         return $normalized;
     }
 
-    protected function _normalizeCutter($number)
+    protected function _normalizeRemainder()
     {
-        $cutter = substr($this->_cutters[$number], 0, 4);
-        return str_pad($cutter, 4, LCCallNumber::LOW_SORT_CHAR, STR_PAD_RIGHT);
+        $remainder = substr($this->_remainder, 0, 5);
+        return str_pad($remainder, 5, LCCallNumber::LOW_SORT_CHAR, STR_PAD_RIGHT);
     }
 
     public function isValid()
@@ -76,17 +79,69 @@ class LCCallNumber
         return $this->_is_valid;
     }
 
-    protected function _parse()
+    public function parse($input_string)
     {
-        if (!isset($this->_input_string)) {
-            throw new \Exception('No LCCN entered');
-        }
 
-        if (!$this->_is_parsed) {
-            $this->_is_valid = $this->_parser->parse($this);
-        }
+        $lccregex = <<<REGEX
+        /^\s*
+        ([A-Z]{1,3})  # alpha
+        \s*
+        (?:         # optional numbers with optional decimal point
+          (\d+)
+          (?:\s*?\.\s*?(\d+))?
+        )?
+        \s*
+        (\d+[stndrh]*)? # optional extra numbering including suffixes (1st, 2nd, etc.)
+            \s*
+            (?:               # optional cutter
+          \.? \s*
+              ([A-Z])      # cutter letter
+              \s*
+              (\d+\w? | \Z)??        # cutter numbers
+        )?
+        \s*
+        (?:               # optional cutter
+          \.? \s*
+              ([A-Z])      # cutter letter
+              \s*
+              (\d+\w | \Z)?        # cutter numbers
+        )?
+        \s*
+        (?:               # optional cutter
+          \.? \s*
+              ([A-Z])      # cutter letter
+              \s*
+              (\d+\w | \Z)?        # cutter numbers
+        )?
+        (\s+.+?)?        # everthing else
+            \s*$
+  /x
+REGEX;
+        $this->_matches = array_fill(0, 12, '');
+        preg_match($lccregex, $input_string, $this->_matches);
 
-        $this->_is_parsed = true;
+        $this->letters = isset($this->_matches[1]) ? $this->_matches[1] : false;
+        $this->number = isset($this->_matches[2]) ? $this->_matches[2] : false;
+        if (isset($this->_matches[3])) {
+            $this->number .= "." . $this->_matches[3];
+        }
+        $this->cutter_1 = $this->_buildCutter(5, 6);
+        $this->cutter_2 = $this->_buildCutter(7, 8);
+        $this->cutter_3 = $this->_buildCutter(9, 10);
+        $this->remainder = isset($this->_matches[11]) ? $this->_matches[11] : false;
+
+        $this->_is_valid = ($this->letters && preg_match(
+                '/^[A-Z]/',
+                $this->letters
+            ) && $this->number && $this->cutter_1);
+    }
+
+    protected function _buildCutter($letter_index, $number_index)
+    {
+        if (isset($this->_matches[$letter_index]) && isset($this->_matches[$number_index])) {
+            return $this->_matches[$letter_index] . $this->_matches[$number_index];
+        }
+        return false;
     }
 
     protected function _setCutter($index, $cutter)
@@ -94,21 +149,8 @@ class LCCallNumber
         $this->_cutters[$index] = $cutter;
     }
 
-    protected function _setInputString($input_string)
-    {
-        if (isset ($this->input_string)) {
-            throw new \Exception("Input string already set");
-        }
-        $this->_input_string = $input_string;
-        $this->_parse($input_string);
-    }
-
     public function __get($name)
     {
-        if ($name == 'input_string') {
-            return $this->_input_string;
-        }
-
         switch ($name) {
             case 'letters':
                 return $this->_letters;
@@ -148,11 +190,6 @@ class LCCallNumber
             case 'remainder':
                 $this->_remainder = $value;
                 break;
-            case 'input_string':
-                $this->_setInputString($value);
-                break;
-            default:
-                throw new \Exception("$name is not an LCCN property");
         }
     }
 }
